1a4fba94f39740196bf8a476c0025d5779388121
Jakub Kreisinger <jakub.kreisinger.024@pslib.cz>
2025-05-21 12:04:05 +0200

    uprava sensitivity

--- main.ts
+++ main.ts
@@ -1,34 +1,44 @@
-radio.setGroup(173)
+radio.setGroup(54)
+radio.setTransmitSerialNumber(true)
 
-let y = 0
 let x = 0
-let zataceni = 0
+let y = 0
+let zatoc = 0
 let rychlost = 0
 
 function controlServo(xTilt: number, yTilt: number) {
-    rychlost = Math.map(yTilt, -1023, 1023, -200, 200)
-    rychlost = Math.constrain(rychlost, 0, 200)
-    zataceni = Math.map(xTilt, -1023, 1023, -200, 200)
-    zataceni = Math.constrain(zataceni, 0, 200)
-    if (yTilt < -10) {
-        // vpřed
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, yTilt + xTilt / 3)
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, yTilt - xTilt / 3)
-    } else if (yTilt > 10) {
-        // pozpátku
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, yTilt)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, yTilt)
+    rychlost = Math.map(xTilt, -1023, 1023, -255, 255)
+    rychlost = Math.constrain(rychlost, 100, 255)
+    zatoc = Math.map(yTilt, 0, 1023, 0, 255)
+    zatoc = Math.constrain(zatoc, 100, 255)
+
+    if (xTilt < -10) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, xTilt + yTilt / 3)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, xTilt - yTilt / 20)
+    } else if (xTilt > 10) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, xTilt)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, xTilt)
     } else {
-        // stop
         PCAmotor.MotorStopAll()
     }
 }
-radio.onReceivedValue(function (name, value) {
-    if (name == "x") {
-        x = value
-    } else if (name == "y") {
-        y = value
+
+
+radio.onReceivedString(function (receivedString: string) {
+    if (receivedString == "Stop") {
+        PCAmotor.MotorStopAll()
+        basic.pause(1000)
+    } else {
+        let parts = receivedString.split(",")
+        if (parts.length == 2) {
+            x = parseInt(parts[0])
+            y = parseInt(parts[1])
+            controlServo(x, y)
+        }
     }
-    controlServo(x, y)
 })
 
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+
+})
