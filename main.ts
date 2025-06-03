196dace6e598aa1aa7c036fe788a5de17983f556
Flayaaa <Flayaaa@outlook.com>
2025-06-03 20:38:41 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,54 @@
 radio.setGroup(54)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(4)
 radio.setTransmitSerialNumber(true)
 
-let x = 0
-let y = 0
-let zatoc = 0
-let rychlost = 0
+let serialNumber: number
+let xd = 0
+let yd = 0
+let tn = 0
+let sd = 0
+let parts: string[] = []
 
-function controlServo(xTilt: number, yTilt: number) {
-    rychlost = Math.map(xTilt, -1023, 1023, -255, 255)
-    rychlost = Math.constrain(rychlost, 100, 255)
-    zatoc = Math.map(yTilt, 0, 1023, 0, 255)
-    zatoc = Math.constrain(zatoc, 100, 255)
-
-    if (xTilt < -10) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, xTilt + yTilt / 3)
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, xTilt - yTilt / 20)
-    } else if (xTilt > 10) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, xTilt)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, xTilt)
+function ServoControl(xdTilt: number, ydTilt: number) {
+    sd = Math.map(xdTilt, -1023, 1023, -200, 200)
+    sd = Math.constrain(sd, 0, 200)
+    tn = Math.map(ydTilt, -1023, 1023, -200, 200)
+    tn = Math.constrain(tn, 0, 200)
+    if (xdTilt < -100) {
+        // vpřed (forward)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, (xdTilt + ydTilt) / 2)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, (xdTilt - ydTilt) / 2)
+    } else if (ydTilt > 100) {
+        // pozpátku (reverse)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, xdTilt)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, xdTilt)
     } else {
+        // stop
         PCAmotor.MotorStopAll()
     }
 }
+serialNumber = radio.receivedPacket(RadioPacketProperty.SerialNumber)
 
-
-radio.onReceivedString(function (receivedString: string) {
-    if (receivedString == "Stop") {
-        PCAmotor.MotorStopAll()
-        basic.pause(1000)
-    } else {
-        let parts = receivedString.split(",")
-        if (parts.length == 2) {
-            x = parseInt(parts[0])
-            y = parseInt(parts[1])
-            controlServo(x, y)
+radio.onReceivedString(function (receivedString) {
+    radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    if (serialNumber = 1569162800) {
+        if (receivedString == "Stop") {
+            PCAmotor.MotorStopAll()
+            basic.pause(1000)
+        } else {
+            parts = receivedString.split(",")
+            if (parts.length == 2) {
+                xd = parseInt(parts[0])
+                yd = parseInt(parts[1])
+                ServoControl(xd, yd)
+            }
         }
     }
 })
 
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
-
+    basic.showIcon(IconNames.Skull)
 })
+
+
