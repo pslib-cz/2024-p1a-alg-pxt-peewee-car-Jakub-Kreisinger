e906769d335f58363d58f929296495ea7c9d67f2
Jakub Kreisinger <jakub.kreisinger.024@pslib.cz>
2025-06-09 09:41:27 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -50,3 +50,17 @@ radio.onReceivedString(function (receivedString) {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Skull)
 })
+
+type IRC = {
+    l: DigitalPin,
+    c: DigitalPin,
+    r: DigitalPin
+}
+const IR: IRC = {
+    l: DigitalPin.P14,
+    c: DigitalPin.P15,
+    r: DigitalPin.P13
+}
+pins.setPull(IR.l, PinPullMode.PullNone);
+pins.setPull(IR.c, PinPullMode.PullNone);
+pins.setPull(IR.r, PinPullMode.PullNone);
